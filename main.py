074d418438c506b13ab996b4dbd0748ed902cbd4
HoolyPanda <pavlov.ss10@gmail.com>
2020-09-04 03:02:40 +0300

    done music and manga

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import os
-import ffmpeg
 import youtube_dl
 import discord
+from threading import Thread
 from discord.ext import commands
+from musicPlayer import MusicPlayer
+import requests
+import bs4
 
 token = open(f'./token.cred').readline()
 testUrl = f'https://www.youtube.com/watch?v=02Tim9kmb3I'
 testId = 698604865634435202
 tortUrl = f'https://www.youtube.com/watch?v=pBqyntzYaoQ'
 
-# a = os.getcwd()
-yt_dlOpts = {
-    'format': 'bestaudio/mp3',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-    'outtmpl': f'{os.getcwd()}/downloads/%(title)s.%(ext)s'
-}
-print(os.getcwd())
-   
+playMusic = False
+currentTrack = ''
+musicFolder = f'{os.getcwd()}/downloads'
+__musicPlayer: Thread
+mP: MusicPlayer
+mP = None
+musicQueue = []
+domain = 'live'
 
-# client = discord.Client()
+
+print(os.getcwd())
+
 client: commands.Bot
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='self.')
 
 
 @client.event
@@ -35,46 +36,89 @@ async def on_ready():
 async def ping(context):
     await context.send(f'Pong')
 
-def __PlayAudio(vClient:discord.VoiceClient):
-    path = f'{os.getcwd()}/downloads'
-    for f in os.listdir(path):
-        if f.split('.')[-1] == 'mp3':
-            audioS = discord.FFmpegPCMAudio(f'{path}/{f}')
-            vClient.play(audioS)
-            b = 0
+@client.command(pass_context=True)
+async def download(context:commands.context, url):
+    id = len(os.listdir(musicFolder))
+    yt_dlOpts = {'format': 'bestaudio/mp3','outtmpl': f'{os.getcwd()}/downloads/{id}.%(title)s.%(ext)s'}
+    with youtube_dl.YoutubeDL(yt_dlOpts) as ydl:
+        ydl.download([url]) 
+    pass
 
 @client.command(pass_context=True)
-async def play(context:commands.context, url):
-    ytDl =  youtube_dl.YoutubeDL()
-    with youtube_dl.YoutubeDL(yt_dlOpts ) as ydl:
-        # ydl.download([testUrl]) 
-        voiceClient: discord.VoiceClient
-        voiceChannel = client.get_channel(context.message.author.voice.channel.id)
-        voiceClient = await voiceChannel.connect()
-        __PlayAudio(voiceClient)
-    await voiceClient.disconnect()
-    pass
+async def play(context:commands.context):
+    voiceChannel = client.get_channel(context.message.author.voice.channel.id)
+    vc = await voiceChannel.connect()
+    global mP
+    mP = MusicPlayer(vc)
+    mP.play()
+
+@client.command(pass_context=True)
+async def pause(context: commands.context):
+    if mP:
+        mP.pause()
+
+@client.command(pass_context=True)
+async def resume(context: commands.context):
+    mP.resume()
+
+@client.command(pass_context=True)
+async def stop(context: commands.context):
+    mP.stop()
+
+@client.command(pass_context=True)
+async def next(context: commands.context):
+    mP.next()
+
+@client.command(pass_context=True)
+async def clear(context: commands.context):
+    mP.clearQueue()
+
+@client.command(pass_context=True)
+async def ls(context: commands.context):
+    msg = ''
+    if mP is None:
+        await context.channel.send(content=f'Очередь пуста')
+    else:
+        if len(mP.queue) == 0:
+            await context.channel.send(content=f'Очередь пуста')
+        else:
+            for i in mP.queue:
+                msg += f'{i}\n'
+            await context.channel.send(content=str(msg))
+
+@client.command(pass_context=True)
+async def manga(context: commands.context):
+    domain = 'live'
+    req = requests.get(f'https://readmanga.{domain}/internal/random')
+    await context.channel.send(content= req.url)
+
+@client.command(pass_context=True)
+async def get_boobs(context: commands.context):
+    req = requests.get(f'https://tits-guru.com/randomTits')
+    # parsedReq = 
+    # await context.channel.send(content= req.url)
+    
+@client.command(pass_context=True)
+async def set_domain(context: commands.context, dmn):
+    domain = dmn
+    await context.channel.send(content= f'https://readmanga.{domain}')
+    
 
 @client.command(pass_context=True)
 async def torture(context:commands.context, name):
     n = name
-    # context.
     print(n)
     for memebr in context.guild.members:
         if memebr.mention == n:
             for c in context.guild.channels:
                 if c.id == testId:
+                    global mP
+                    if not mP is None:
+                        mP.clearQueue()
+                    await download(context, tortUrl)
                     await memebr.move_to(c)
                     voiceClient = await client.get_channel(testId).connect()
-                    ytDl =  youtube_dl.YoutubeDL()
-                    with youtube_dl.YoutubeDL(yt_dlOpts) as ydl:
-                        # ydl.download([tortUrl]) 
-                        __PlayAudio(voiceClient)
-            # await voiceClient.disconnect()
-            z = 0
-    a = 0
+                    mP = MusicPlayer(voiceClient)
+                    mP.play()
 
 client.run(token)
-
-
-# client.connect()
---
+++ musicPlayer.py
@@ -0,0 +1,76 @@
+import discord
+import os
+from threading import Thread
+
+class MusicPlayer():
+    def __init__(self, voiceClient: discord.VoiceClient):
+        self.voiceClient = voiceClient
+        self.currentTrack = ''
+        self.currentAudioSource = None
+        self.musicFolder = f'{os.getcwd()}/downloads'
+        self.queueBlacklist = []
+        self.queue = os.listdir(self.musicFolder)
+        self.queueThread: Thread
+        self.isPaused = False
+        self.stopPlaying = False
+    
+    def __PlayQueue(self):
+        while len(self.queue) > 0 and not self.stopPlaying:
+            if not self.voiceClient.is_playing() and not self.isPaused:
+                try:
+                    self.queue.pop(0)
+                    os.remove(f'{self.musicFolder}/{self.currentTrack}')
+                    if len(self.queue) > 0:
+                        self.currentTrack = self.queue[0]
+                        self.currentAudioSource = discord.FFmpegPCMAudio(f'{self.musicFolder}/{self.currentTrack}')
+                        if not self.voiceClient.is_playing():
+                            self.voiceClient.play(self.currentAudioSource)
+                except Exception as e:
+                    break
+
+    def play(self):
+        if len(self.queue) > 0:
+            self.currentTrack = self.queue[0]
+            self.currentAudioSource = discord.FFmpegPCMAudio(f'{self.musicFolder}/{self.currentTrack}')
+            if not self.voiceClient.is_playing():
+                self.voiceClient.play(self.currentAudioSource)
+                self.queueThread = Thread(target=self.__PlayQueue)
+                self.queueThread.start()
+
+    def pause(self):
+        self.isPaused = True
+        self.voiceClient.pause()
+    
+    def resume(self):
+        self.voiceClient.play(self.currentAudioSource)
+        self.isPaused = False
+    
+    def stop(self):
+        self.stopPlaying = True
+        self.isPaused = True
+        self.voiceClient.stop()
+        self.currentTrack = ''
+        self.stopPlaying = False
+        self.isPaused = False
+
+    def clearQueue(self):
+        self.stop()
+        for t in self.queue:
+            os.remove(f'{self.musicFolder}/{t}')
+        self.queue = []
+        self.currentAudioSource = None
+        self.currentTrack = ''
+
+    def updateQueue(self):
+        self.queue = []
+        pass
+
+    def deleteTrack(self, id):
+
+
+        pass
+
+    def next(self):
+        self.stop()
+        self.queue.pop(0)
+        self.play()
